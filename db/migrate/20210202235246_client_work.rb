9e3c239ff9087f6ad392c6651ad70dee8d53ae78
Bruno <pelli.br@gmail.com>
2021-02-02 20:54:21 -0300

    Atualizado

---
+++ db/migrate/20210202235246_client_work.rb
@@ -0,0 +1,4 @@
+class ClientWork < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_29_011857) do
+ActiveRecord::Schema.define(version: 2021_02_02_235246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
