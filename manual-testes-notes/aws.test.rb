1174422d381556043532bc6f57c7177aad3d0fdf
Bruno Pellizzetti <pelli.br@gmail.com>
2021-06-13 08:40:55 -0300

    Delete manual-testes-notes directory
    deletando pasta desnecessária 
    adicionado no .gitignore

--- manual-testes-notes/aws.test.rb
+++
@@ -1,9 +0,0 @@
-def amazon_client
- require 'aws-sdk-s3'
-  aws_config = Aws.config.update({region: 'us-west-2', credentials: Aws::Credentials.new(
-      ENV['AWS_ID'],
-      ENV['AWS_SECRET_KEY']
-      )})
-  @aws_client = Aws::S3::Client.new
-  @s3 = Aws::S3::Resource.new(region: 'us-west-2')
-end
--- manual-testes-notes/linhas-gerais.md
+++
@@ -1,5 +0,0 @@
-# Linhas Gerais do Projeto
-- Focar mais no documento do que no sistema;
-- Criar documento -> Enviar para o cliente;
-- Tentar automatizar este fluxo;
-- Criar formas melhores de criar os documentos;
