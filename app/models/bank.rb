9bca9cb79cbf2225c5f3fddb3a8e321711035b65
Bruno Pellizzetti <pelli.br@gmail.com>
2021-06-27 11:16:33 -0300

    corrigido model bank -- [campo vazio]

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,3 +1,19 @@
 class Bank < ApplicationRecord
   belongs_to :client, dependent: :destroy
+
+  # Retirar NIL
+  NULL_ATTRS = %w( name agency account)
+  before_save :fill_if_nil
+
+
+  protected
+
+  def fill_if_nil
+    NULL_ATTRS.each { |attr| self[attr] = "[Campo Vazio]" if self[attr].nil? }
+  end
+
+
+
+
+
 end
