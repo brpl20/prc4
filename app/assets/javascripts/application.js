07ccd9adf1096982bfa4abb21b5ebb0a70c54037
Bruno Pellizzetti <pelli.br@gmail.com>
2021-10-28 08:08:22 -0300

    js bugs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,15 +32,32 @@ $(document).ready(function(){
 
 
   let input = document.getElementById("client_zip");
+  let inputAdressStreet = document.getElementById("client_address");
+  let inputAdressCity = document.getElementById("client_city");
+  let inputAdressState = document.getElementById("client_state");
+  let focus = document.getElementById("client_bank_attributes_name");
+
+
+
+
   input.addEventListener('input', function () {
     if(input.value.length == 10){
-      console.log(input.value);
+      // console.log(input.value);
       let cepClean = input.value.replace("-", "").replace(".", "");
       fetch(`https://viacep.com.br/ws/${cepClean}/json/ `)
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(function(data)
+          {
+            // console.log(data);
+            inputAdressStreet.value = data.logradouro + ", "
+            inputAdressCity.value = data.localidade
+            inputAdressState.value = data.uf
+            inputAdressStreet.focus()
+
+
+          });
     }
-    console.log(input.value.length);
+    // console.log(input.value.length);
   });
 
 
