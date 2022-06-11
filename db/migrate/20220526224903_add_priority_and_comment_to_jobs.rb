1b6a577bf73c26b16801747c5e0fbeafd8da7636
Bruno Pellizzetti <pelli.br@gmail.com>
2022-06-11 17:00:59 -0300

    priority comment - schema bug

--- db/migrate/20220526224903_add_priority_and_comment_to_jobs.rb
+++ db/migrate/20220526224903_add_priority_and_comment_to_jobs.rb
@@ -3,7 +3,7 @@
 class AddPriorityAndCommentToJobs < ActiveRecord::Migration[6.1]
   def change
     add_reference :jobs, :work, null: true, foreign_key: true
-    # add_column :jobs, :priority, :integer
-    # add_column :jobs, :comment, :string
+    add_column :jobs, :priority, :integer
+    add_column :jobs, :comment, :string
   end
 end
