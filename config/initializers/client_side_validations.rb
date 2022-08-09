7b3f3254956d3d94ef7c8270895ad6833cf0fff9
Jemisson <jemison321@gmail.com>
2022-08-09 00:07:17 -0400

    Altertações para a validação do usuário

--- config/initializers/client_side_validations.rb
+++ config/initializers/client_side_validations.rb
@@ -20,6 +20,9 @@ ActionView::Base.field_error_proc = proc do |html_tag, instance|
   if html_tag =~ /^<label/
     %(<div class="field_with_errors">#{html_tag}</div>).html_safe
   else
+    p '==============================='
+    p html_tag
+    p '--------------------------------------'
     %(<div class="field_with_errors">#{html_tag}<label for="#{instance.send(:tag_id)}" class="message">#{instance.error_message.first}</label></div>).html_safe
   end
 end
