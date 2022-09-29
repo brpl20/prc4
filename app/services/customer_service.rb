8b5749ed2bb645aa3f56fa206f07fe44f197e5f5
Jemisson <jemison321@gmail.com>
2022-09-28 20:57:26 -0400

    Cria customer client

--- app/services/customer_service.rb
+++ app/services/customer_service.rb
@@ -3,12 +3,20 @@
 # service para controle dos metodos de customers
 class CustomerService
   class << self
+
     def create_customer(client)
-      p 'criando ****************8'
-      Customer.create!(
+      customer = Customer.create!(
         email: client.emails.first.email,
         password: 'Cliente123#',
         password_confirmation: 'Cliente123#'
+      )
+      create_customer_client(client, customer)
+    end
+
+    def create_customer_client(client, customer)
+      CustomerClient.create!(
+        client: client,
+        customer: customer
       )
     end
   end
