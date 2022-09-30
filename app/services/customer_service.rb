44a144dde9c5dbd2d652a764d7126a15adec16a8
Jemisson <jemison321@gmail.com>
2022-09-29 20:11:48 -0400

    Ajuste de próximos passos em área do cliente

--- app/services/customer_service.rb
+++ app/services/customer_service.rb
@@ -24,10 +24,13 @@ class CustomerService
     def data_for_home(customer)
       # Precisamos criar uma tela principal para o customer acessar e escolher qual cliente ele vai ver os processos;
 
+      # a definir
       # Customer acessa dashboard via endpoint /cliente;
       # Se customer PF visualiza cards de clientes e seleciona algum;
-        # Visulualiza /area_cliente/:client_id/trabalhos do cliente selecionado
+        # Visualiza /area_cliente/:client_id/trabalhos do cliente selecionado
       # Se customer PF visulualiza /area_cliente/:client_id/trabalhos;
+
+      # criar regras de negocio para visualizacao dos representantes / representados
     end
   end
 end
